e3c0dbc7168fceb354aa4f9aa64e47e2f66ac542
NeerajNero <neerajnero9066@gmail.com>
2025-02-13 15:44:51 +0530

    updated code for production

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://email-draft-generator.vercel.app/"
 }))
 initializeDatabase()
 app.get('/', (req,res) => {
