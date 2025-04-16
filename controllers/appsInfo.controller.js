476395dc98710781053d516a89cbf011e19f6387
NeerajNero <neerajnero9066@gmail.com>
2025-04-16 21:41:31 +0530

    added code for fetching appData

--- controllers/appsInfo.controller.js
+++ controllers/appsInfo.controller.js
@@ -30,3 +30,20 @@ export const getAllAppsInfo = async(req,res) => {
         res.status(500).json({message: "Internal server error", error: error.message})
     }
 }
+
+export const getAppDataByPlatform = async(req,res) => {
+    try{
+        const {platform} = req.params
+        if(!platform){
+            return res.status(400).json("platform is required!")
+        }
+        const getAllAppsData = await AppsInfo.find({platform})
+        if(!getAllAppsData || getAllAppsData.length === 0){
+            return res.status(404).json({message: "No apps data found"})
+        }
+        res.status(200).json({message: "all apps data fetched successfully.", appsData: getAllAppsData})
+    }catch(error){
+        console.log("error occured while fetching appsData", error.message)
+        res.status(500).json({message: "Internal server error", error: error.message})
+    }
+}
